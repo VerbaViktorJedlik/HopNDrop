78c7917aa2bc6eb5ed8a063c2d46aba0668a0049
Préts Barnabás <prets.barnabas@gmail.com>
2025-04-14 11:03:04 +0200

    take

--- backend/src/controllers/package.controller.ts
+++ backend/src/controllers/package.controller.ts
@@ -1,17 +1,30 @@
 import { Request, Response } from "express";
-import { findPackage } from "../db/package";
+import { findPackage, updatePackage } from "../db/package";
 import { Package } from "@prisma/client";
 import { PackageResponse } from "@common";
 
 export class PackageController {
 
     static async take(req: Request<{id:string}>, res: Response<PackageResponse>) {
-    let pkg : Package[] | null = await findPackage(req.params.id);
-    if(pkg == null){
+    let pkgs = await findPackage(req.params.id);
+    if(pkgs == null){
         res.status(404).json({ result: "Error", msg: "Nem létezik csomag ilyen azonosítóval."});
+        return;
     }
-    if(pkg![0].status != "Waiting" ){
+    let pkg = pkgs[0];
+
+    if(pkg.status != "Waiting" ){
         res.status(400).json({result: "Error", msg: "Nem megfelelő a csomag státusza."});
+        return;
     }
+    pkg.status = "EnRoute"
+    const updatedPkg = await updatePackage(pkg);
+
+    if (!updatedPkg) {
+        res.status(400).json({result: "Error", msg: "Nem sikerült frissíteni a csomag státuszát."});
+        return;
+    }
+    
+    res.status(200).json({result: "Success", ...updatedPkg});
 }
 }
