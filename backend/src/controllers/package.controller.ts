61e57507735779ea9729766c9551ecd85f47a65d
Préts Barnabás <prets.barnabas@gmail.com>
2025-04-14 11:05:05 +0200

    deliver, recieve

--- backend/src/controllers/package.controller.ts
+++ backend/src/controllers/package.controller.ts
@@ -24,6 +24,51 @@ export class PackageController {
         res.status(400).json({result: "Error", msg: "Nem sikerült frissíteni a csomag státuszát."});
         return;
     }
+
+    res.status(200).json({result: "Success", ...updatedPkg});
+}
+
+static async deliver(req: Request<{id:string}>, res: Response<PackageResponse>) {
+    let pkgs = await findPackage(req.params.id);
+    if(pkgs == null){
+        res.status(404).json({ result: "Error", msg: "Nem létezik csomag ilyen azonosítóval."});
+        return;
+    }
+    let pkg = pkgs[0];
+
+    if(pkg.status != "EnRoute" ){
+        res.status(400).json({result: "Error", msg: "Nem megfelelő a csomag státusza."});
+        return;
+    }
+    pkg.status = "Delivered"
+    const updatedPkg = await updatePackage(pkg);
+
+    if (!updatedPkg) {
+        res.status(400).json({result: "Error", msg: "Nem sikerült frissíteni a csomag státuszát."});
+        return;
+    }
+    
+    res.status(200).json({result: "Success", ...updatedPkg});
+}
+static async recive(req: Request<{id:string}>, res: Response<PackageResponse>) {
+    let pkgs = await findPackage(req.params.id);
+    if(pkgs == null){
+        res.status(404).json({ result: "Error", msg: "Nem létezik csomag ilyen azonosítóval."});
+        return;
+    }
+    let pkg = pkgs[0];
+
+    if(pkg.status != "Delivered" ){
+        res.status(400).json({result: "Error", msg: "Nem megfelelő a csomag státusza."});
+        return;
+    }
+    pkg.status = "Completed"
+    const updatedPkg = await updatePackage(pkg);
+
+    if (!updatedPkg) {
+        res.status(400).json({result: "Error", msg: "Nem sikerült frissíteni a csomag státuszát."});
+        return;
+    }
     
     res.status(200).json({result: "Success", ...updatedPkg});
 }
