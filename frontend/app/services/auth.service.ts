dbd6e0ad6984f9aea87bc188b620298585508f75
Szaloki Peter <szaloki.peter.krisztian@students.jedlik.eu>
2025-04-14 11:59:19 +0200

    hibakezeles

--- frontend/app/services/auth.service.ts
+++ frontend/app/services/auth.service.ts
@@ -4,55 +4,72 @@ import { ConfigService } from "./config.service";
 export class AuthService{
 
     static async registerUser(username:string,password:string): Promise<PublicSelf | null>{
-        const response = await fetch("http://localhost:3000/api/auth/register",{
-            method: "POST",
-            headers:{
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({username:username,password:password})
-        })
-
-        const result: AuthResponse = await response.json()
-        if (result.result == "Success") {
-            ConfigService.setToken(result.self.jwt)
-            return result.self
+        try {
+            const response = await fetch("http://localhost:3000/api/auth/register",{
+                method: "POST",
+                headers:{
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({username:username,password:password})
+            })
+    
+            const result: AuthResponse = await response.json()
+            if (result.result == "Success") {
+                ConfigService.setToken(result.self.jwt)
+                return result.self
+            }
+    
+            return null
+        } catch (error) {
+            console.log(error)
+            return null
         }
-
-        return null
     }
 
     static async login(username:string,password:string): Promise<PublicSelf | null>{
-        const response = await fetch("http://localhost:3000/api/auth/login",{
-            method: "POST",
-            headers:{
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({username:username,password:password})
-        })
-
-        const result: AuthResponse = await response.json()
-        if (result.result == "Success") {
-            ConfigService.setToken(result.self.jwt)
-            return result.self;
+        try {
+            const response = await fetch("http://localhost:3000/api/auth/login",{
+                method: "POST",
+                headers:{
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({username:username,password:password})
+            })
+    
+            const result: AuthResponse = await response.json()
+            if (result.result == "Success") {
+                ConfigService.setToken(result.self.jwt)
+                return result.self;
+            }
+    
+            return null
+        } catch (error) {
+            console.log(error)
+            return null
         }
 
-        return null
     }
 
     static async verifyToken(): Promise<boolean>{
-        const response = await fetch("http://localhost:3000/api/auth/validate",{
-            method: "POST",
-            headers:{
-                "Content-Type": "application/json",
-                "Authorization": ConfigService.getToken()!
+        try {
+            const response = await fetch("http://localhost:3000/api/auth/validate",{
+                method: "POST",
+                headers:{
+                    "Content-Type": "application/json",
+                    "Authorization": ConfigService.getToken()!
+                }
+            })
+    
+            const result: TokenVerifyResponse = await response.json()
+            if (result == "Success") {
+                return true
             }
-        })
-
-        const result: TokenVerifyResponse = await response.json()
-        if (result == "Success") {
-            return true
+    
+            return false
+        } catch (error) {
+            console.log(error)
+            return false
         }
 
-        return false
     }
 }
--- frontend/app/services/package.service.ts
+++ frontend/app/services/package.service.ts
@@ -4,17 +4,23 @@ import { ConfigService } from "./config.service";
 export class PackageService{
 
     static async TakePackage(id:string): Promise<PublicPackage | null>{
-        const response = await fetch(`http://localhost:3000/api/package/${id}/take`,{
-            method: "POST",
-            headers:{
-                "Content-Type": "application/json",
-                "Authorization": ConfigService.getToken()!
+        try {
+            const response = await fetch(`http://localhost:3000/api/package/${id}/take`,{
+                method: "POST",
+                headers:{
+                    "Content-Type": "application/json",
+                    "Authorization": ConfigService.getToken()!
+                }
+            })
+            const result: PackageResponse = await response.json()
+            if (result.result == "Success") {
+                return result.package
             }
-        })
-        const result: PackageResponse = await response.json()
-        if (result.result == "Success") {
-            return result.package
+            return null
+        } catch (error) {
+            console.log(error)
+            return null
         }
-        return null
+
     }
 }
