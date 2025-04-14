024eb425a019a3efbd3b9288faa2e95b7ba997f7
Préts Barnabás <prets.barnabas@gmail.com>
2025-04-14 10:17:01 +0200

    asd

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,4 +2,13 @@ import express from "express"
 
 export const router = express.Router()
 
+router.post("/package/:id/take", take)
+router.post("/package/:id/deliver", deliver)
+router.post("/package/:id/revice", recive)
+
+
+async function name(params:type) {
+    
+}
+
 export default router
