1f488a8f31e35f49afd75046924a60b354ba86d3
Szaloki Peter <szaloki.peter.krisztian@students.jedlik.eu>
2025-04-14 12:37:52 +0200

    package stuff

--- frontend/app/services/package.service.ts
+++ frontend/app/services/package.service.ts
@@ -3,30 +3,11 @@ import { ConfigService } from "./config.service";
 
 export class PackageService{
 
-    static async TakePackage(id:string): Promise<PublicPackage | null>{
-        try {
-            const response = await fetch(`http://localhost:3000/api/package/${id}/take`,{
-                method: "POST",
-                headers:{
-                    "Content-Type": "application/json",
-                    "Authorization": ConfigService.getToken()!
-                }
-            })
-            const result: PackageResponse = await response.json()
-            if (result.result == "Success") {
-                return result.package
-            }
-            return null
-        } catch (error) {
-            console.log(error)
-            return null
-        }
 
-    }
 
     static async GetAllPackages(): Promise<PublicPackage[] | null>{
         try {
-            const response = await fetch(`http://localhost:3000/api/package/get`,{
+            const response = await fetch(`${ConfigService.apiUrl}/package/get`,{
                 method: "GET",
                 headers:{
                     "Content-Type": "application/json",
@@ -46,7 +27,7 @@ export class PackageService{
 
     static async GetPackageById(id: number): Promise<PublicPackage | null>{
         try {
-            const response = await fetch(`http://localhost:3000/api/package/get/${id}`,{
+            const response = await fetch(`${ConfigService.apiUrl}/package/get/${id}`,{
                 method: "GET",
                 headers:{
                     "Content-Type": "application/json",
@@ -55,6 +36,70 @@ export class PackageService{
             })
             const result: PackageResponse = await response.json()
             if(result.result == "Success"){
+                return result.package
+            }
+            console.log(result.msg)
+            return null
+        } catch (error) {
+            console.log(error)
+            return null
+        }
+    }
+
+    static async TakePackage(id:string): Promise<PublicPackage | null>{
+        try {
+            const response = await fetch(`${ConfigService.apiUrl}/package/${id}/take`,{
+                method: "POST",
+                headers:{
+                    "Content-Type": "application/json",
+                    "Authorization": ConfigService.getToken()!
+                }
+            })
+            const result: PackageResponse = await response.json()
+            if (result.result == "Success") {
+                return result.package
+            }
+            console.log(result.msg)
+            return null
+        } catch (error) {
+            console.log(error)
+            return null
+        }
+
+    }
+
+    static async ReceivePackage(id:string): Promise<PublicPackage | null>{
+        try {
+            const response = await fetch(`${ConfigService.apiUrl}/package/${id}/recieve`,{
+                method: "POST",
+                headers:{
+                    "Content-Type": "application/json",
+                    "Authorization": ConfigService.getToken()!
+                }
+            })
+            const result: PackageResponse = await response.json()
+            if (result.result == "Success") {
+                return result.package
+            }
+            console.log(result.msg)
+            return null
+        } catch (error) {
+            console.log(error)
+            return null
+        }
+    }
+
+    static async DeliverPackage(id:string): Promise<PublicPackage | null>{
+        try {
+            const response = await fetch(`${ConfigService.apiUrl}/package/${id}/deliver`,{
+                method: "POST",
+                headers:{
+                    "Content-Type": "application/json",
+                    "Authorization": ConfigService.getToken()!
+                }
+            })
+            const result: PackageResponse = await response.json()
+            if (result.result == "Success") {
                 return result.package
             }
             console.log(result.msg)
