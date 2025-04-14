7dae40543c96efd1e231ef9e72410cbc3d15ad4b
Préts Barnabás <prets.barnabas@gmail.com>
2025-04-14 10:34:10 +0200

    asdadasdasd

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,14 +1,23 @@
-import express from "express"
+import express, { Request, Response } from "express"
+import { findPackage } from "./db/package"
+import { Package } from "@prisma/client";
 
 export const router = express.Router()
 
-// router.post("/package/:id/take", take)
-// router.post("/package/:id/deliver", deliver)
-// router.post("/package/:id/revice", recive)
+router.post("/package/:id/take", take)
+//router.post("/package/:id/deliver", deliver)
+//router.post("/package/:id/recieve", recive)
 
 
-async function create(req: Request, res: Response) {
-
+async function take(req: Request<{id:string}>, res: Response) {
+    let pkg : Package[] | null = await findPackage(req.params.id);
+    if(pkg == null){
+        res.status(404).json({"message": "Nem létezik csomag ilyen azonosítóval."});
+    }
+    if(pkg![0].status != "Waiting" ){
+        res.status(400).json({"message": "Nem megfelelő a csomag státusza."});
+    }
+    
 }
 
 export default router
