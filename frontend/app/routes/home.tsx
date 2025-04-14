388036071a58221e469ee557543cf5f93d373ec8
Szabó Taddeus <szabo.taddeus@students.jedlik.eu>
2025-04-14 11:22:02 +0200

    profile

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -1,34 +1,14 @@
-import { PostageTable } from "components/table";
-import { Button } from "~/components/ui/button";
-import { Fragment } from "react/jsx-runtime";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "~/components/ui/card";
-import { Link } from "react-router";
-export default function Home() {
-  return (
-    <div className="p-10">
-      <Card>
-        <CardHeader>
-          <CardTitle>Csomagjaid</CardTitle>
-          <CardDescription>Itt láthatod csomagjaid.</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <PostageTable />
-        </CardContent>
-        <div className="buttons flex justify-center gap-4 mt-5 ">
-          <Button asChild>
-            <Link to="/send">Új csomag feladása</Link>
-          </Button>
-          <Button>
-            <Link to="/pickup">Új csomag elvállalása</Link>
-          </Button>
-        </div>
-      </Card>
-    </div>
-  );
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router";
+
+function home() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate("/profile");
+  }, [navigate]);
+
+  return null;
 }
+
+export default home;
